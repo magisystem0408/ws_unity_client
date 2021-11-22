825e77b56c7418d6d85e75befcf453ff8952091d
magisystem0408 <masato.matsudo@gmail.com>
2021-11-22 12:50:51 +0900

    Initalcommit

---
+++ Assets/Plugins/Client.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class Client : MonoBehaviour
+{
+    private WebSocket ws;
+    
+    void Start()
+    {
+        ws = new WebSocket("ws://localhost:3000/");
+        ws.OnOpen += (sender, e) => { Debug.Log("websocket Open"); };
+
+        ws.OnMessage += (sender, e) => {};
+
+        ws.OnError += (sender, e) => { Debug.Log("Websocket Error Massage" + e.Message); };
+
+        ws.OnClose += (sender, e) => { Debug.Log("WebSocket Close"); };
+
+        ws.Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("s"))
+        {
+            ws.Send("Test Message");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        ws.Close();
+        ws = null;
+    }
+}
